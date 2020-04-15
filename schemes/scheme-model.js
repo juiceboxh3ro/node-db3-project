37383bcdaf8e82275002c00aab826e5ef66f839a
Jesse <fkng3rmz@gmail.com>
2020-04-15 18:38:36 -0400

    mvp

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -14,14 +14,17 @@ function findById(id) {
 
 function findSteps(id) {
   /*
-    SELECT sch.id, sch.scheme_name, st.step_number, st.instructions
+    SELECT sch.scheme_name, st.step_number, st.instructions
     FROM schemes AS sch
-    JOIN steps AS st ON sch.id = st.scheme_id
-    ORDER BY sch.id, st.step_number
+    JOIN steps AS st ON st.scheme_id = {id}
+    WHERE sch.id = {id}
   */
 
-  return db('schemes').where({ id }).first()
-  .then(() => db('steps').where({ scheme_id: id }))
+  return db('schemes')
+  .join('steps', 'steps.scheme_id', 'schemes.id')
+  .select('steps.id', 'schemes.scheme_name', 'steps.step_number', 'steps.instructions')
+  .where('schemes.id', id)
+  .orderBy('steps.step_number')
 }
 
 function add(scheme) {
